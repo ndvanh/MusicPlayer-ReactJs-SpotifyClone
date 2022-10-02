4c33dbc0c67cd23ec4789e34768899e43af1c1d6
VietAnh <ndvietanh01122001@gmail.com>
2022-10-02 16:27:57 +0700

    update Artists

--- src/pages/Lib/LibArtists.js
+++ src/pages/Lib/LibArtists.js
@@ -3,9 +3,10 @@ import {uniqAuthor} from '../../utils/Utlis'
 import ArtistInfo from '../../components/MusicContent/ArtistInfo'
 
 export default function Lib_artists() {
+  const artistList = uniqAuthor
   return (
     <div className=''>
-          <ArtistInfo dataArtist={uniqAuthor}/>
+          <ArtistInfo dataArtist={artistList}/>
     </div> 
   )
 }
